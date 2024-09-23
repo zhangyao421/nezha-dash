dcf926951cf5de044132782a75d948dd4353385a
zhangyao421 <180969052+zhangyao421@users.noreply.github.com>
2024-09-24 00:36:09 +0800

    Update footer.tsx

--- app/(main)/footer.tsx
+++ app/(main)/footer.tsx
@@ -1,10 +1,14 @@
 export default function Footer() {
+  const startYear = 2024;
+  const currentYear = new Date().getFullYear();
+
   return (
-    <footer className="mx-auto w-full max-w-5xl">
+    <footer className="mx-auto w-full max-w-5xl flex justify-center items-center">
       <section className="flex flex-col">
         <section className="mt-1 flex items-center gap-2 text-[13px] font-light tracking-tight text-neutral-600/50 dark:text-neutral-300/50">
-          © {new Date().getFullYear()}{" "}
-          <a href={"https://musifei.live"}>@Musifei</a>
+         Copyright © {startYear}
+          {currentYear > startYear && `-${currentYear}`}{" "}
+          <a href={"https://musifei.live"}>Musifei</a>
         </section>
       </section>
     </footer>
